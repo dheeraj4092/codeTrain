436f0dcd8d2cfbe6705fc841167588bcd4f06001
DHEERAJ KUMAR <dheerajkumar29@DHEERAJs-MacBook-Air.local>
2024-12-05 03:21:51 +0530

    deploy latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/codeTrain/',
   plugins: [react()],
-  
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
